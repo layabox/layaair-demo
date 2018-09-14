8aee97dd5a8173bf351898820ffba751371a75b7
tianjing <544200536@qq.com>
2018-09-14 16:24:57 +0800

    更新 layanative相关的文件

--- h5/conchInit.js
+++ h5/conchInit.js
@@ -1,120 +1,127 @@
-﻿var list=[
-	"Animation_Altas",
+﻿function GetQueryString(name){
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+}
+
+var id = GetQueryString("id") || 0;
+
+var list = [
+	"Animation_Altas", // 0
 	"Animation_SWF",
 	"BlendMode_Lighter",
 	"Debug_FPSStats",
 	"Filters_Blur",
-	"Filters_Color",
+	"Filters_Color", // 5
 	"Filters_Glow",
 	"Interaction_CustomEvent",
 	"Interaction_Drag",
 	"Interaction_FixInteractiveRegion",
-	"Interaction_Hold",
+	"Interaction_Hold", // 10
 	"Interaction_Keyboard",
 	"Interaction_Mouse",
 	"Interaction_Rotate",
 	"Interaction_Scale",
-	"Interaction_Swipe",
+	"Interaction_Swipe", // 15
 	"Loader_MultipleType",
 	"Loader_ProgressAndErrorHandle",
 	"Loader_Sequence",
 	"Loader_SingleType",
-	"Network_GET",
+	"Network_GET", // 20
 	"Network_POST",
 	"Network_ProtocolBuffer",
 	"Network_Socket",
 	"Network_XML",
-	"Particle_T1",
+	"Particle_T1", // 25
 	"Particle_T2",
 	"Particle_T3",
 	"PerformanceTest_Cartoon",
 	"PerformanceTest_Legend",
-	"PerformanceTest_Maggots",
+	"PerformanceTest_Maggots", // 30
 	"PerformanceTest_Skeleton",
 	"Physics_Cloth",
 	"Physics_NewtonsCradle",
 	"Physics_Slingshot",
-	"SmartScale_Align_Contral",
+	"SmartScale_Align_Contral", // 35
 	"SmartScale_Landscape",
 	"SmartScale_Portrait",
 	"SmartScale_Scale_EXTRACT_FIT",
 	"SmartScale_Scale_NOBORDER",
-	"SmartScale_Scale_NOSCALE",
+	"SmartScale_Scale_NOSCALE", // 40
 	"SmartScale_Scale_SHOW_ALL",
 	"SmartScale_T",
 	"Sound_SimpleDemo",
 	"Sprite_Cache",
-	"Sprite_Container",
+	"Sprite_Container", // 45
 	"Sprite_DisplayImage",
 	"Sprite_DrawPath",
 	"Sprite_DrawShapes",
 	"Sprite_MagnifyingGlass",
-	"Sprite_NodeControl",
+	"Sprite_NodeControl", // 50
 	"Sprite_Pivot",
 	"Sprite_RoateAndScale",
 	"Sprite_SwitchTexture",
 	"Text_AutoSize",
-	"Text_BitmapFont",
+	"Text_BitmapFont", // 55
 	"Text_ComplexStyle",
 	"Text_Editable",
 	"Text_HTML",
 	"Text_InputMultiline",
-	"Text_InputSingleline",
+	"Text_InputSingleline", // 60
 	"Text_MaxChars",
 	"Text_Overflow",
 	"Text_Prompt",
 	"Text_Restrict",
-	"Text_Scroll",
+	"Text_Scroll", // 65
 	"Text_Underline",
 	"Text_WordWrap",
 	"TiledMap_AnimationTile",
 	"TiledMap_IsometricWorld",
-	"TiledMap_PerspectiveWall",
+	"TiledMap_PerspectiveWall", // 70
 	"TiledMap_ScrollMap",
 	"Timer_CallLater",
 	"Timer_DelayExcute",
 	"Timer_Interval",
-	"Tween_EaseFunctionsDemo",
+	"Tween_EaseFunctionsDemo", // 75
 	"Tween_Letters",
 	"Tween_SimpleSample",
 	"Tween_TimeLine",
 	"UI_Button",
-	"UI_CheckBox",
+	"UI_CheckBox", // 80
 	"UI_Clip",
 	"UI_ColorPicker",
 	"UI_ComboBox",
 	"UI_Dialog",
-	"UI_Image",
+	"UI_Image", // 85
 	"UI_Input",
 	"UI_Label",
 	"UI_List",
 	"UI_ProgressBar",
-	"UI_RadioGroup",
+	"UI_RadioGroup", // 90
 	"UI_ScrollBar",
 	"UI_Slider",
 	"UI_Tab",
 	"UI_TextArea",
-	"UI_Tree",
+	"UI_Tree", // 95
 	"InputDevice_Map",
 	"InputDevice_Media",
 	"InputDevice_Shake",
 	"InputDevice_Video",
-	"PerformanceTest_Cartoon2",
+	"PerformanceTest_Cartoon2", // 100
 	"PIXI_Example_04",
 	"PIXI_Example_05",
 	"PIXI_Example_21",
 	"PIXI_Example_23",
-	"Skeleton_ChangeSkin",
+	"Skeleton_ChangeSkin", // 105
 	"Skeleton_MultiTexture",
 	"Skeleton_SpineEvent",
 	"Skeleton_SpineIkMesh",
 	"Skeleton_SpineStretchyman",
-	"Skeleton_SpineVine",
+	"Skeleton_SpineVine", // 110
 	"Sprite_Guide"
  ];
 
- 
- var url="samples/"+list[id]+".js";
+ var url="2d/js/"+list[id]+".js";
  var xhr = new XMLHttpRequest();
  xhr.open('GET',url,true);
  xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded"); 
@@ -123,6 +130,7 @@
  }
 //if(window.conch)
 //PerfShow(true);
+
  xhr.onload=function(e){
     
      window._mouselist=[];
@@ -144,26 +152,24 @@
 		  return false;
 		}
 	 }
-	 window.conch&&conch.showLoadingView(false);
+	 // window.conch&&conch.showLoadingView(false);
      window.eval(xhr.responseText);
 	 var mousetype=('ontouchstart' in window)?"touchstart":"mousedown";
-	 function jiba()
+
+	
+
+	 function loadNewPage()
 	 {
-	  type++;
-		 if(type>2)
-		 {
-		    id++;
-			type=0;
-		 }
+		id++
 		var pre=location.protocol+"//"+location.host+location.pathname;
-		 var url=pre+"?id="+id+"&type="+type;
-		 console.log(">>>>>>>@@@@@@@@@@@@@@@="+url);
-	     window.location.href=url;
+		var url=pre+"?id="+id;
+		console.log(">>>>>>>@@@@@@@@@@@@@@@="+url);
+	    window.location.href=url;
 	 }
 	 document.addEventListener(mousetype,function(e){
 	   if(addMouse(Date.now()))
 	   {
-	    jiba();
+			loadNewPage();
 	   }
 	 
 	 });
@@ -173,7 +179,7 @@
     switch (e.keyCode) {
 
         case 82:
-            jiba();
+            loadNewPage();
             break;
     }
 });
