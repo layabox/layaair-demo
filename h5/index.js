6feb4324ae9f094c2fdf85d0abe385925341c4b6
tianjing <544200536@qq.com>
2018-10-25 14:56:18 +0800

    1 添加index.js,index3d.js文件,用于runtime的运行

---
+++ h5/index.js
@@ -0,0 +1,14 @@
+loadLib("libs/bytebuffer.js");
+loadLib("libs/protobuf.js");
+loadLib("libs/laya.core.js");
+loadLib("libs/laya.webgl.js");
+loadLib("libs/laya.filter.js");
+loadLib("libs/laya.particle.js");
+loadLib("libs/laya.ani.js");
+loadLib("libs/laya.ui.js");
+loadLib("libs/laya.html.js");
+loadLib("libs/laya.tiledmap.js");
+loadLib("libs/laya.device.js");
+loadLib("conchInit.js");
+
+window.screenOrientation = "sensor_landscape";
---
+++ h5/index3d.js
@@ -0,0 +1,23 @@
+loadLib("libs/laya.core.js");
+loadLib("libs/laya.physics3D.js");
+loadLib("libs/laya.webgl.js");
+loadLib("libs/laya.filter.js");
+loadLib("libs/laya.particle.js");
+loadLib("libs/laya.ani.js");
+loadLib("libs/laya.ui.js");
+loadLib("libs/laya.html.js");
+loadLib("libs/laya.tiledmap.js");
+loadLib("libs/laya.d3.js");
+loadLib("libs/laya.d3Plugin.js");
+loadLib("libs/laya.pathfinding.js");
+loadLib("3d/js/common/CameraMoveScript.js");
+loadLib("3d/js/common/VRCameraMoveScript.js");
+loadLib("3d/js/common/Tool.js");
+loadLib("3d/js/common/TriggerCollisionScript.js");
+loadLib("3d/js/shaderModule/customMaterials/CustomMaterial.js");
+loadLib("3d/js/shaderModule/customMaterials/CustomTerrainMaterial.js");
+loadLib("res/threeDimen/pbrlut.js");
+loadLib("conchInit3d.js");
+
+
+window.screenOrientation = "sensor_landscape";
