80c0939076e9b70d1a4bf7d6f94a9e79135e322c
ZhengQixv <102980547@qq.com>
2018-11-21 23:17:31 +0800

    zqx--sky

--- h5/3d/ts/skyModule/SkyDomeSample.ts
+++ h5/3d/ts/skyModule/SkyDomeSample.ts
@@ -1,4 +1,5 @@
 class SkyDomeSample {
+	var camera:Laya.Camera;
     constructor() {
         Laya3D.init(0, 0);
         Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
@@ -6,17 +7,21 @@ class SkyDomeSample {
         Laya.Stat.show();
         var scene:Laya.Scene3D = Laya.stage.addChild(new Laya.Scene3D()) as Laya.Scene3D;
         
-        var camera:Laya.Camera = scene.addChild(new Laya.Camera(0, 0.1, 100)) as Laya.Camera;
-        camera.transform.rotate(new Laya.Vector3( 10, 0, 0), true, false);
-        camera.addComponent(CameraMoveScript);
-        camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
+        this.camera = scene.addChild(new Laya.Camera(0, 0.1, 100)) as Laya.Camera;
+        this.camera.transform.rotate(new Laya.Vector3( 10, 0, 0), true, false);
+        this.camera.addComponent(CameraMoveScript);
+        this.camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
         
         //天空盒
         Laya.BaseMaterial.load("../../res/threeDimen/skyBox/DawnDusk/SkyBox.lmat", Laya.Handler.create(null, function(mat:Laya.SkyBoxMaterial):void {
-            camera.skyboxMaterial = mat;
-            var exposureNumber:number = 0;
+			var skyRenderer = new Laya.SkyRenderer();
+		    skyRenderer.mesh = Laya.SkyBox.instance;
+		    skyRenderer.material = mat;
+		    this.camera.skyRenderer = skyRenderer;
+		    Laya.timer.frameLoop(1, this, this.onFrameLoop);
             Laya.timer.frameLoop(1, this, function():void {
-                mat.exposure = Math.sin(exposureNumber += 0.01) + 1;
+				this.camera.skyRenderer.material.exposure = Math.sin(this.exposureNumber += 0.01) + 1;
+		        this.camera.skyRenderer.material.rotation += 0.01;
             });
         }))
     }
