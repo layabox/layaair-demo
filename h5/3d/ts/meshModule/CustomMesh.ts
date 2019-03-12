ecb82fa69692667583ca2d28bf133ad0b9c477a4
ZhengQixv <1029805476@qq.com>
2019-03-12 13:51:44 +0800

    zqx--更正示例代码ts版本CustomMesh代码错误

--- h5/3d/ts/meshModule/CustomMesh.ts
+++ h5/3d/ts/meshModule/CustomMesh.ts
@@ -1,78 +1,118 @@
 import CameraMoveScript from "./common/CameraMoveScript"
 import Tool from "./common/Tool"
-class MeshLoad {
-    private sprite3D:Laya.Sprite3D;
+class CustomMesh {
+	private sprite3D:Laya.Sprite3D;
     private lineSprite3D:Laya.Sprite3D;
-	private rotation:Laya.Vector3 = new Laya.Vector3(0, 0.01, 0);
-	private curStateIndex:number = 0;
-	private changeActionButton:Laya.Button;
-    
-     constructor() {
-        
-        Laya3D.init(0, 0);
-        Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
-        Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
-        Laya.Stat.show();
-        
-        var scene:Laya.Scene3D = Laya.stage.addChild(new Laya.Scene3D()) as Laya.Scene3D;
-        
-        var camera:Laya.Camera = scene.addChild(new Laya.Camera(0, 0.1, 100)) as Laya.Camera;
-        camera.transform.translate(new Laya.Vector3(0, 0.8, 1.5));
-        camera.transform.rotate(new Laya.Vector3(-15, 0, 0), true, false);
-        
-        var directionLight:Laya.DirectionLight = scene.addChild(new Laya.DirectionLight()) as Laya.DirectionLight;
-        directionLight.color = new Laya.Vector3(0.6, 0.6, 0.6);
-        
-        this.sprite3D = scene.addChild(new Laya.Sprite3D()) as Laya.Sprite3D;
-        this.lineSprite3D = scene.addChild(new Laya.Sprite3D()) as Laya.Sprite3D;
-        
-        Laya.Mesh.load("../res/threeDimen/skinModel/LayaMonkey/Assets/LayaMonkey/LayaMonkey-LayaMonkey.lm", Laya.Handler.create(this, function(mesh:Laya.Mesh):void {
-            var layaMonkey:Laya.MeshSprite3D = this.sprite3D.addChild(new Laya.MeshSprite3D(mesh)) as Laya.MeshSprite3D;
-            layaMonkey.transform.localScale = new Laya.Vector3(0.3, 0.3, 0.3);
-            layaMonkey.transform.rotation = new Laya.Quaternion(0.7071068, 0, 0, -0.7071067);
-            var layaMonkeyLineSprite3D:Laya.PixelLineSprite3D = this.lineSprite3D.addChild(new Laya.PixelLineSprite3D(5000)) as Laya.PixelLineSprite3D;
-            Tool.linearModel(layaMonkey, layaMonkeyLineSprite3D, Laya.Color.GREEN);
-            
-            var plane:Laya.MeshSprite3D = this.sprite3D.addChild(new Laya.MeshSprite3D(Laya.PrimitiveMesh.createPlane(6, 6, 10, 10))) as Laya.MeshSprite3D;
-            plane.transform.position = new Laya.Vector3(0, 0, -1);
-            var planeLineSprite3D:Laya.PixelLineSprite3D = this.lineSprite3D.addChild(new Laya.PixelLineSprite3D(1000)) as Laya.PixelLineSprite3D;
-            Tool.linearModel(plane, planeLineSprite3D, Laya.Color.GRAY);
-            
-            
-            Laya.timer.frameLoop(1, this, function():void {
-                layaMonkeyLineSprite3D.transform.rotate(this.rotation, false);
-                layaMonkey.transform.rotate(this.rotation, false);
-            });
-            
-            this.lineSprite3D.active = false;
-            this.loadUI();
-        }));
-    
-    }
-    
-   
-    
-    private loadUI():void {
-        Laya.loader.load(["../res/threeDimen/ui/button.png"], Laya.Handler.create(this, function():void {
-            var changeActionButton:Laya.Button = Laya.stage.addChild(new Laya.Button("../res/threeDimen/ui/button.png", "正常模式")) as Laya.Button;
-            changeActionButton.size(160, 40);
-            changeActionButton.labelBold = true;
-            changeActionButton.labelSize = 30;
-            changeActionButton.sizeGrid = "4,4,4,4";
-            changeActionButton.scale(Laya.Browser.pixelRatio, Laya.Browser.pixelRatio);
-            changeActionButton.pos(Laya.stage.width / 2 - changeActionButton.width * Laya.Browser.pixelRatio / 2, Laya.stage.height - 100 * Laya.Browser.pixelRatio);
-            changeActionButton.on(Laya.Event.CLICK, this, function():void {
-                if (++this.curStateIndex % 2 == 1) {
-                    this.sprite3D.active = false;
-                    this.lineSprite3D.active = true;
-                    changeActionButton.label = "网格模式";
-                } else {
-                    this.sprite3D.active = true;
-                    this.lineSprite3D.active = false;
-                    changeActionButton.label = "正常模式";
-                }
-            });
-        }));
-    }
-}
-new MeshLoad;
+    private curStateIndex = 0;
+		
+	constructor() {
+			
+		Laya.Shader3D.debugMode = true;
+		Laya3D.init(0, 0);
+		Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
+		Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
+		Laya.Stat.show();
+			
+		var scene = Laya.stage.addChild(new Laya.Scene3D());
+			
+		var camera = new Laya.Camera(0, 0.1, 100);
+		scene.addChild(camera);
+		camera.transform.translate(new Laya.Vector3(0, 2, 5));
+		camera.transform.rotate(new Laya.Vector3(-15, 0, 0), true, false);
+		camera.addComponent(CameraMoveScript);
+		camera.clearColor = new Laya.Vector4(0.2, 0.2, 0.2, 1.0);
+			
+		var directionLight = new Laya.DirectionLight();
+		scene.addChild(directionLight);
+		directionLight.transform.worldMatrix.setForward(new Laya.Vector3(1.0, -1.0, -1.0));
+			
+		this.sprite3D = new Laya.Sprite3D();
+		scene.addChild(this.sprite3D);
+		this.lineSprite3D = new Laya.Sprite3D();
+		scene.addChild(this.lineSprite3D);
+			
+		//正方体
+		var box = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createBox(0.5, 0.5, 0.5));
+		this.sprite3D.addChild(box);
+		box.transform.position = new Laya.Vector3(2.0, 0.25, 0.6);
+		box.transform.rotate(new Laya.Vector3(0, 45, 0), false, false);
+		//为正方体添加像素线渲染精灵
+		var boxLineSprite3D = new Laya.PixelLineSprite3D(100);
+		this.lineSprite3D.addChild(boxLineSprite3D);
+		//设置像素线渲染精灵线模式
+		Tool.linearModel(box, boxLineSprite3D, Laya.Color.GREEN);
+			
+		//球体
+		var sphere = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createSphere(0.25, 20, 20));
+		this.sprite3D.addChild(sphere);
+		sphere.transform.position = new Laya.Vector3(1.0, 0.25, 0.6);
+		var sphereLineSprite3D = new Laya.PixelLineSprite3D(3500);
+		this.lineSprite3D.addChild(sphereLineSprite3D);
+		Tool.linearModel(sphere, sphereLineSprite3D, Laya.Color.GREEN);
+			
+		//圆柱体
+		var cylinder = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createCylinder(0.25, 1, 20));
+		this.sprite3D.addChild(cylinder);
+		cylinder.transform.position = new Laya.Vector3(0, 0.5, 0.6);
+		var cylinderLineSprite3D = new Laya.PixelLineSprite3D(1000);
+		this.lineSprite3D.addChild(cylinderLineSprite3D);
+		Tool.linearModel(cylinder, cylinderLineSprite3D, Laya.Color.GREEN);
+			
+		//胶囊体
+		var capsule = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createCapsule(0.25, 1, 10, 20));
+		this.sprite3D.addChild(capsule);
+		capsule.transform.position = new Laya.Vector3(-1.0, 0.5, 0.6);
+		var capsuleLineSprite3D = new Laya.PixelLineSprite3D(3000);
+		this.lineSprite3D.addChild(capsuleLineSprite3D);
+		Tool.linearModel(capsule, capsuleLineSprite3D, Laya.Color.GREEN);
+			
+		//圆锥体
+		var cone = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createCone(0.25, 0.75));
+		this.sprite3D.addChild(cone);
+		cone.transform.position = new Laya.Vector3(-2.0, 0.375, 0.6);
+		var coneLineSprite3D = new Laya.PixelLineSprite3D(500);
+		this.lineSprite3D.addChild(coneLineSprite3D);
+		Tool.linearModel(cone, coneLineSprite3D, Laya.Color.GREEN);
+		
+			
+		//平面
+		var plane = new Laya.MeshSprite3D(Laya.PrimitiveMesh.createPlane(6, 6, 10, 10));
+		this.sprite3D.addChild(plane);
+		var planeLineSprite3D = new Laya.PixelLineSprite3D(1000);
+		this.lineSprite3D.addChild(planeLineSprite3D);
+		Tool.linearModel(plane, planeLineSprite3D, Laya.Color.GRAY);
+			
+		this.lineSprite3D.active = false;
+		this.loadUI();
+		}
+		
+		
+		
+		private loadUI():void {
+			
+			Laya.loader.load(["../res/threeDimen/ui/button.png"], Laya.Handler.create(this, function():void {
+				
+				var changeActionButton = new Laya.Button("../res/threeDimen/ui/button.png", "正常模式");
+				Laya.stage.addChild(changeActionButton);
+				changeActionButton.size(160, 40);
+				changeActionButton.labelBold = true;
+				changeActionButton.labelSize = 30;
+				changeActionButton.sizeGrid = "4,4,4,4";
+				changeActionButton.scale(Laya.Browser.pixelRatio, Laya.Browser.pixelRatio);
+				changeActionButton.pos(Laya.stage.width / 2 - changeActionButton.width * Laya.Browser.pixelRatio / 2, Laya.stage.height - 100 * Laya.Browser.pixelRatio);
+				changeActionButton.on(Laya.Event.CLICK, this, function():void {
+					if (++this.curStateIndex % 2 == 1) {
+						this.sprite3D.active = false;
+						this.lineSprite3D.active = true;
+						changeActionButton.label = "网格模式";
+					} else {
+						this.sprite3D.active = true;
+						this.lineSprite3D.active = false;
+						changeActionButton.label = "正常模式";
+					}
+				});
+			}));
+		}
+	}
+
+	new CustomMesh;
