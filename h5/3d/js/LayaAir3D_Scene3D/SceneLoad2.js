f3998a8541a2e215a2e792bd7263ec8e70796d4c
ZhengQixv <1029805476@qq.com>
2019-04-18 14:23:32 +0800

    zqx--场景加载二示例代码更新了js和ts

--- h5/3d/js/LayaAir3D_Scene3D/SceneLoad2.js
+++ h5/3d/js/LayaAir3D_Scene3D/SceneLoad2.js
@@ -1,26 +1,66 @@
-class SceneLoad2{
-    constructor(){
-        Laya.Shader3D.debugMode = true;
-        Laya3D.init(0, 0);
-        Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
-        Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
-        Laya.Stat.show();
-		Laya.Scene3D.load("res/threeDimen/scene/TerrainScene/XunLongShi.ls", Laya.Handler.create(null, this.sceneLoadFinished));
-    }
-
-    sceneLoadFinished(scene){
-        Laya.stage.addChild(scene);
-        let camera = scene.getChildByName("Main Camera");
-        camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
-        camera.addComponent(CameraMoveScript);
-        Laya.BaseMaterial.load("res/threeDimen/skyBox/skyBox3/skyBox3.lmat", Laya.Handler.create(null, function (mat) {
-            camera.skyboxMaterial = mat;
-        }));
-        scene.getChildByName('Scenes').getChildByName('HeightMap').active = false;
-        scene.getChildByName('Scenes').getChildByName('Area').active = false;
-    }
+class SceneLoad2 {
+	constructor() {
+		//初始化引擎
+		Laya3D.init(0, 0);
+		Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
+		Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
+		//显示性能面板
+		Laya.Stat.show();
+		
+		Laya.Scene3D.load("res/threeDimen/scene/TerrainScene/XunLongShi.ls", Laya.Handler.create(this, function(scene) {
+			Laya.stage.addChild(scene);
+			//开启雾化效果
+			scene.enableFog = true;
+			//设置雾化的颜色
+			scene.fogColor = new Laya.Vector3(0, 0, 0.6);
+			//设置雾化的起始位置，相对于相机的距离
+			scene.fogStart = 10;
+			//设置雾化最浓处的距离。
+			scene.fogRange = 40;
+			//设置场景环境光
+			scene.ambientColor = new Laya.Vector3(0.6, 0, 0);
+			
+			//获取场景中的相机
+			var camera = scene.getChildByName("Main Camera");
+			//设置相机横纵比
+			camera.aspectRatio = 0;
+			//设置相机近距裁剪
+			camera.nearPlane = 0.1;
+			//设置相机远距裁剪
+			camera.farPlane = 1000;
+			//相机设置清楚标记
+			camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
+			//设置摄像机视野范围（角度）
+			camera.fieldOfView = 60;
+			//设置背景颜色
+			//camera.clearColor = new Vector4(0,0,0.6,1);    
+			//加入摄像机移动控制脚本
+			camera.addComponent(CameraMoveScript);
+			
+			//加载相机天空盒材质
+			Laya.BaseMaterial.load("res/threeDimen/skyBox/skyBox3/SkyBox.lmat", Laya.Handler.create(this, function(mat) {
+				var skyRenderer = camera.skyRenderer;
+				skyRenderer.mesh = Laya.SkyBox.instance;
+				skyRenderer.material = mat;
+			}));
+			
+			//创建方向光
+			var light = scene.addChild(new Laya.DirectionLight());
+			//移动灯光位置
+			light.transform.translate(new Laya.Vector3(0, 2, 5));
+			//调整灯光方向
+			var mat = light.transform.worldMatrix;
+			mat.setForward(new Laya.Vector3(0, -5, 1));
+			light.transform.worldMatrix=mat;
+			//设置灯光漫反射颜色
+			light.diffuseColor = new Laya.Vector3(0.3, 0.3, 0.3);
+			
+			//激活场景中的两个子节点
+			(scene.getChildByName('Scenes').getChildByName('HeightMap')).active = false;
+			(scene.getChildByName('Scenes').getChildByName('Area')).active = false;
+		}));
+	
+	}
 }
 
-
-//激活启动类
 new SceneLoad2();
--- h5/3d/ts/LayaAir3D_Scene3D/SceneLoad2.ts
+++ h5/3d/ts/LayaAir3D_Scene3D/SceneLoad2.ts
@@ -1,28 +1,67 @@
 import CameraMoveScript from "./common/CameraMoveScript"
-class TerrainScene {
-    constructor() {
-        Laya.Shader3D.debugMode = true;
-        Laya3D.init(0, 0);
-        Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
-        Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
-        Laya.Stat.show();
-        
-    	Laya.Scene3D.load("res/threeDimen/scene/TerrainScene/XunLongShi.ls", Laya.Handler.create(null, function(scene:Laya.Scene3D):void {
-            Laya.stage.addChild(scene);
-            var camera:Laya.Camera = scene.getChildByName("Main Camera") as Laya.Camera;
-            camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
-            camera.addComponent(CameraMoveScript);
-            
-            Laya.BaseMaterial.load("res/threeDimen/skyBox/skyBox3/skyBox3.lmat", Laya.Handler.create(null, function(mat:Laya.BaseMaterial):void {
-				var skyRenderer:Laya.SkyRenderer = camera.skyRenderer;
+class SceneLoad2 {
+	constructor() {
+		//初始化引擎
+		Laya3D.init(0, 0);
+		Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
+		Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
+		//显示性能面板
+		Laya.Stat.show();
+		
+		Laya.Scene3D.load("res/threeDimen/scene/TerrainScene/XunLongShi.ls", Laya.Handler.create(this, function(scene:Laya.Scene3D):void {
+			Laya.stage.addChild(scene);
+			//开启雾化效果
+			scene.enableFog = true;
+			//设置雾化的颜色
+			scene.fogColor = new Laya.Vector3(0, 0, 0.6);
+			//设置雾化的起始位置，相对于相机的距离
+			scene.fogStart = 10;
+			//设置雾化最浓处的距离。
+			scene.fogRange = 40;
+			//设置场景环境光
+			scene.ambientColor = new Laya.Vector3(0.6, 0, 0);
+			
+			//获取场景中的相机
+			var camera = scene.getChildByName("Main Camera") as Laya.Camera;
+			//设置相机横纵比
+			camera.aspectRatio = 0;
+			//设置相机近距裁剪
+			camera.nearPlane = 0.1;
+			//设置相机远距裁剪
+			camera.farPlane = 1000;
+			//相机设置清楚标记
+			camera.clearFlag = Laya.BaseCamera.CLEARFLAG_SKY;
+			//设置摄像机视野范围（角度）
+			camera.fieldOfView = 60;
+			//设置背景颜色
+			//camera.clearColor = new Vector4(0,0,0.6,1);    
+			//加入摄像机移动控制脚本
+			camera.addComponent(CameraMoveScript);
+			
+			//加载相机天空盒材质
+			Laya.BaseMaterial.load("res/threeDimen/skyBox/skyBox3/SkyBox.lmat", Laya.Handler.create(this, function(mat:Laya.BaseMaterial):void {
+				var skyRenderer = camera.skyRenderer;
 				skyRenderer.mesh = Laya.SkyBox.instance;
 				skyRenderer.material = mat;
-            }));
-            
-            (scene.getChildByName('Scenes').getChildByName('HeightMap') as Laya.MeshSprite3D).active = false;
-            (scene.getChildByName('Scenes').getChildByName('Area') as Laya.MeshSprite3D).active = false;
-        }))
-    }
-    
+			}));
+			
+			//创建方向光
+			var light = scene.addChild(new Laya.DirectionLight()) as Laya.DirectionLight;
+			//移动灯光位置
+			light.transform.translate(new Laya.Vector3(0, 2, 5));
+			//调整灯光方向
+			var mat:Laya.Matrix4x4 = light.transform.worldMatrix;
+			mat.setForward(new Laya.Vector3(0, -5, 1));
+			light.transform.worldMatrix=mat;
+			//设置灯光漫反射颜色
+			light.diffuseColor = new Laya.Vector3(0.3, 0.3, 0.3);
+			
+			//激活场景中的两个子节点
+			(scene.getChildByName('Scenes').getChildByName('HeightMap') as Laya.MeshSprite3D).active = false;
+			(scene.getChildByName('Scenes').getChildByName('Area') as Laya.MeshSprite3D).active = false;
+		}));
+	
+	}
 }
-new TerrainScene;
+
+new SceneLoad2();
