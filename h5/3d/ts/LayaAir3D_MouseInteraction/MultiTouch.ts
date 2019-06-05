39608f240247279b8c3a0dedc5a83b6bfcdac77c
ZhengQixv <1029805476@qq.com>
2019-06-05 17:33:38 +0800

    zqx--多点触控示例代码ts更新

--- h5/3d/ts/LayaAir3D_MouseInteraction/MultiTouch.ts
+++ h5/3d/ts/LayaAir3D_MouseInteraction/MultiTouch.ts
@@ -1,6 +1,8 @@
 class MultiTouch{
 	private text:Laya.Text;
+	private infoText:Laya.Text;
 	private _upVector3:Laya.Vector3 = new Laya.Vector3(0, 1, 0);
+	
     constructor(){
 		Laya.alertGlobalError = true;
         this._upVector3 = new Laya.Vector3(0, 1, 0);
@@ -10,7 +12,7 @@ class MultiTouch{
 		Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
 			
 		//预加载所有资源
-		var resource = [{url: "res/threeDimen/skinModel/LayaMonkey/LayaMonkey.lh", type: Laya3D.HIERARCHY, priority: 1}];
+		var resource = ["res/threeDimen/skinModel/LayaMonkey/LayaMonkey.lh"];
 		Laya.loader.create(resource, Laya.Handler.create(this, this.onComplete));
     }
     onComplete(){
@@ -40,23 +42,31 @@ class MultiTouch{
 		//设置相机的观察目标为小猴子
 		camera.transform.lookAt(monkey.transform.position, new Laya.Vector3(0, 1, 0));
 			
-		//设置文本显示框位置
-		this.text.x = Laya.stage.width / 2 -50 ;
+        //设置文本显示框位置
+        this.text = new Laya.Text();
+		this.text.x = Laya.stage.width / 2 -100 ;
+		this.text.y = 50;
 		this.text.text = "触控点归零";
-		//显示文本显示框
-		this.text = new Laya.Text();
 		this.text.name = "ceshi";
 		this.text.overflow = Laya.Text.HIDDEN;
 		this.text.color = "#FFFFFF";
 		this.text.font = "Impact";
 		this.text.fontSize = 20;
 		this.text.borderColor = "#FFFF00";
-		this.text.x = Laya.stage.width / 2;
 		Laya.stage.addChild(this.text);
+
+		//设置操作提示框
+		this.infoText = new Laya.Text();
+		this.infoText.x = Laya.stage.width / 2 - 100;
+		this.infoText.text = "单指旋转，双指缩放";
+		this.infoText.overflow = Laya.Text.HIDDEN;
+		this.infoText.color = "#FFFFFF";
+		this.infoText.font = "Impact";
+		this.infoText.fontSize = 20;
+		this.infoText.borderColor = "#FFFF00";
+		Laya.stage.addChild(this.infoText);
     }
 }
-//激活启动类
-new MultiTouch();
 class MonkeyScript extends Laya.Script3D{
 	private _scene:Laya.Scene3D;
 	private _text:Laya.Text;
@@ -69,6 +79,7 @@ class MonkeyScript extends Laya.Script3D{
 	private isTwoTouch:boolean;
 	private first:boolean;
 	private twoFirst:boolean;
+	private sprite3DSacle:Laya.Vector3 = new Laya.Vector3();
     constructor(){
 		super();
         this._scene = null;
@@ -101,16 +112,16 @@ class MonkeyScript extends Laya.Script3D{
 			//是否为新一次触碰，并未发生移动
 			if (this.first){
 				//获取触碰点的位置
-				this.lastPosition.x = touch._position.x;
-				this.lastPosition.y = touch._position.y;
+				this.lastPosition.x = touch.position.x;
+				this.lastPosition.y = touch.position.y;
 				this.first = false;
 			}
 			else{
 				//移动触碰点
-				var deltaY = touch._position.y - this.lastPosition.y;
-				var deltaX = touch._position.x - this.lastPosition.x;
-				this.lastPosition.x = touch._position.x;
-				this.lastPosition.y = touch._position.y;
+				var deltaY = touch.position.y - this.lastPosition.y;
+				var deltaX = touch.position.x - this.lastPosition.x;
+				this.lastPosition.x = touch.position.x;
+				this.lastPosition.y = touch.position.y;
 				//根据移动的距离进行旋转
 				(this.owner as Laya.Sprite3D).transform.rotate(new Laya.Vector3(1 * deltaY /2, 1 * deltaX / 2, 0), true, false);
 			}
@@ -127,6 +138,7 @@ class MonkeyScript extends Laya.Script3D{
 				this.disVector1.x = touch.position.x - touch2.position.x;
 				this.disVector1.y = touch.position.y - touch2.position.y;
 				this.distance = Laya.Vector2.scalarLength(this.disVector1);
+				this.sprite3DSacle = (this.owner as Laya.Sprite3D).transform.scale;
 				this.twoFirst = false;
 			}
 			else{
@@ -134,7 +146,11 @@ class MonkeyScript extends Laya.Script3D{
 				this.disVector2.y = touch.position.y - touch2.position.y;
 				var distance2 = Laya.Vector2.scalarLength(this.disVector2);
 				//根据移动的距离进行缩放
-				this._camera.transform.translate(new Laya.Vector3(0, 0, -0.01 * (distance2 - this.distance)));
+				let factor =  0.001 * (distance2 - this.distance);
+				this.sprite3DSacle.x += factor;
+				this.sprite3DSacle.y += factor;
+				this.sprite3DSacle.z += factor;
+				(this.owner as Laya.Sprite3D).transform.scale = this.sprite3DSacle;
 				this.distance = distance2;
 			}	
 		}
